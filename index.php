7a4e25886937e992f08f8931b657f22cf746394a
hostingphp <kartinikotakisaran11@gmail.com>
2024-07-18 14:36:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erafone</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link
+    rel="stlyeshett"
+    />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><img src="img/erafone.jpg" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-right" id="navbarText">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Beranda
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>  <a class="nav-link" href="#sejarah">Sejarah</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kategori">Kategori</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Produk
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              
+              <li class="dropdown"><a href="#"><span>IPAD</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="ipadip.php">IPHONE</a></li>
+                  <li><a href="ipadsamsung.php">SAMSUNG</a></li>
+                  <li><a href="ipadoppo.php">OPPO</a></li>
+                  
+                </ul>
+              </li>
+
+              <li class="dropdown"><a href="#"><span>LAPTOP</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="lapip.php">IPHONE</a></li>
+                  <li><a href="lapsam.php">SAMSUNG</a></li>
+                  <li><a href="lapacer.php">ACER</a></li>
+                  <li><a href="lapasus.php">ASUS</a></li>
+
+                  
+                </ul>
+              </li>
+             
+              <li class="dropdown"><a href="#"><span>SMARTPHONE</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="hpip.php">IPHONE</a></li>
+                  <li><a href="hpsam.php">SAMSUNG</a></li>
+                  <li><a href="hpoppo.php">OPPO</a></li>
+                  <li><a href="hpvivo.php">VIVO</a></li>
+
+                  
+                </ul>
+              </li>
+              
+              <li class="dropdown"><a href="#"><span>SMARTWATCH</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="smartwatch.php">SMARTWATCH</a></li>
+                  
+                  
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>ACCESSORIES</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="hand.php">ACCESSORIES HANDPHONE</a></li>
+                  <li><a href="top.php">ACCESSORIES LAPTOP</a></li>
+                  
+                  
+                </ul>
+              </li>
+              
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+      <!-- navbar-end -->
+      
+      <!-- banner -->
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/04102022-bi-jat-26-erafone-5.jpg" class="d-block w-100" alt="first slide">
+          </div>
+          <div class="carousel-item">
+            <img src="img/phone.jpg" class="d-block w-100" alt="second slide">
+          </div>
+          <div class="carousel-item">
+            <img src="img/ERAFONEE.jpeg" class="d-block w-100" alt="third slide ">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!-- banner-end -->
+      <!-- vidio -->
+      <video width="1300px" height="600px" controls class="vid">
+        <source src="img/WhatsApp Video 2024-05-01 at 15.15.06.mp4" type="video/mp4">
+      </video>
+      
+      <!-- end vidio -->
+      
+
+      <!-- Sejarah -->
+      <div class="container-fluid pt-5 pb-5">
+        <div class="container">
+          <h2 class="display-3 text-center" id="sejarah">Sejarah</h2>
+         <section class="bg-light py-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4 mb-3">
+                <img src="img/erafone-megastore-1091x_.webp" class="img-fluid  mx-auto d-block" height="300">
+
+              </div>
+              <div class="col-md-8">
+                
+                <p>   Sejarah Penting Erafone</p>
+                 PT Erajaya Swasembada Tbk (ERAA) mulai menjalankan kegiatan bisnisnya pada 8 Oktober 1996 p;eh Nudiarto Halim dengan akta pendirian No.7 di notaris Myra Yuwono. Perusahaan ini lalu disahkan oleh Menteri Kehakiman pada tahun 1997. Berkantor pusat di Jl. Gedong Panjang No. 29-31, Jakarta, Indonesia, perusahaan memiliki 1,682 gerai di 89 lokasi distribusi tersebar di Malaysia, Indonesia dan Singapura. 
+                  
+                  PT Erajaya Swasembada Tbk (ERAA) mulai menjalankan kegiatan bisnisnya pada 8 Oktober 1996 p;eh Nudiarto Halim dengan akta pendirian No.7 di notaris Myra Yuwono. Perusahaan ini lalu disahkan oleh Menteri Kehakiman pada tahun 1997. Berkantor pusat di Jl. Gedong Panjang No. 29-31, Jakarta, Indonesia, perusahaan memiliki 1,682 gerai di 89 lokasi distribusi tersebar di Malaysia, Indonesia dan Singapura.
+                     ERAA mengakuisisi Teletama Artha Mandiri di tahun 2011. Hal ini membuat perusahaan memperoleh keuntungan sebesar 300%. Di akhir tahunnya, ERAA mengubah statusnya sebagai perusahaan terbuka dan membangun 8 outlet dengan nama Erafone Megastore yang tersebar di wilayah Indonesia.
+                  
+                     Melalui Data Citra Mandiri, perusahaan mengakuisisi iBox atau perusahaan ritel produk Apple. Di tahun yang sama, Erajaya Swasembada menghadirkan toko online bernama erafone.com. Setahun kemudian, perusahaan memiliki jumlah saham penjualan sebanyak 32%. Perusahaan melebarkan sayapnya dengan melakukan kegiatan ekspansi di seluruh wilayah Indonesia
+                  
+                 88 pusat distribusi serta 423 pusat ritel di 17 provinsi di Indonesia mulai menjalankan kegiatan usahanya pada bulan Juni 2013. Kegiatan ini berhasil mendapatkan 18.000 reseller atau penjual pihak ketiga. Kerjasama dilakukan dengan perusahaan Apple, Acer, BlackBerry, Huawei, Asus Dell, Lenovo, HTC, Nokia, Motorola, LG, Samsung serta Sony.
+                  
+                  
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+          <!-- sejarah-end -->
+
+          <!-- kategori -->
+      <div class="container-fluid layanan pt-5 pb-5">
+        <div class="container text-center ">
+          <h2 class="display-3" id="kategori">Kategori</h2>
+          <p>Beberapa Kategori yang Dibuat oleh ERAFONE</p>
+          <div class="row pt-3 ">
+            <div  class="col-md-4">
+              <a class="col-md-4" href="smartwatch.html"><img src="img/WhatsApp Image 2024-06-04 at 14.11.24.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="400"> </a>
+              <h4 class="mt-3">SMARTWATCH</h4>
+           </div>
+           <div class="col-md-4">
+            <a class="col-md-4" href="ipadip.html"><img src="img/WhatsApp Image 2024-06-04 at 14.26.59.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="400"> </a>
+            <h4 class="mt-3 ">IPAD</h4>
+         </div>
+         <div class="col-md-4">
+          <a class="col-md-4" href="hpip.html"><img src="img/WhatsApp Image 2024-06-04 at 14.40.56.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="300"> </a>
+          <h4 class="mt-3">SMARPHONE</h3>
+       </div>
+       <div class="col-md-4 pt-5">
+        <a class="col-md-4" href="lapip.html"><img src="img/WhatsApp Image 2024-06-04 at 14.41.52.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="300"> </a>
+        <h4 class="mt-3">LAPTOP</h4>
+     </div>
+     <div class="col-md-4 pt-5">
+      <a class="col-md-4" href="top.html"><img src="img/WhatsApp Image 2024-06-04 at 15.05.01.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="300"> </a>
+      <h4 class="mt-3">ACCESSORIES LAPTOP</h4>
+    </div>    
+   <div class="col-md-4 pt-5">
+    <a class="col-md-4" href="hand.html"><img src="img/WhatsApp Image 2024-06-04 at 14.58.40.jpeg"class="img-fluid layanan  mx-auto d-block" width="300" height="300"> </a>
+    <h4 class="mt-3">ACCESSORIES HP</h4>
+ </div> 
+         
+         </div>
+        </div>
+      </div>
+
+      <div class="container-fluid pt-5 pb-5">
+        <div class="container">
+         <section class="bg-light py-5">
+            <div class="row">
+              <div class="col-md-4 mb-3">
+                <img src="img/WhatsApp Image 2024-06-03 at 20.53.48.jpeg" class="img-fluid  mx-auto d-block" width="350" height="550">
+              </div>
+              <div class="col-md-8">
+                <h2>MENGAPA HARUS BERBELANJA DI ERAFONE</h2>
+                <p>Ada banyak alasan mengapa Erafone merupakan tempat terpercaya untuk berbelanja gadget via online. Pertama, harga yang ditawarkan kompetitif. Kedua, adanya garansi resmi. Ketiga, banyaknya brand yang memilih Erafone.com untuk mengadakan promo eksklusif.</p>
+                <p>Erafone.com ikut berkontribusi dengan menghadirkan diskon heboh pembelian gadget. Ada cashback hingga Rp 2 juta untuk pembelian gadget dari brand ternama seperti Samsung, Vivo, Oppo dan Iphone.</p> 
+
+                 
+                  
+               
+                 
+              </div>
+            </div>
+            <div class="clearfix pt-5 "> 
+            <img src="img/WhatsApp Image 2024-06-03 at 20.27.06.jpeg" 
+            class="col-md-6 float-md-end mb-3" 
+            width="350" 
+            height="550">
+            <h2>CARA PEMESANAN DI ERAFONE</h2>
+            <p>Nah pada pemesanan di ERAFONE sekarang sangat lah mudah mengingat dengan perkembangan zaman yang semakin canggih maka ERAFONE juga membuat fitur pemesanan dengan mudah pada website ERAFONE  </p>
+            <p>Tidak hanya untuk melakukan pemesan bahkan ERAFONE sekarng menyediakan via chat meskipun hanya bertanya tanya tentang produk produk terbaru pada ERAFONE</p>
+            <p>Untuk lebih jelas bagaimana dengan langkah langkah pemesanan dan chat pada erafone Mimin akan memberikan arahan berikut:</p>
+              <p> * Pada saat membuka website Erafone beberapa detik akan muncul icon berwarna merah dan berbentuk pesan</p>
+                <p>* Anda cukup mengklik icon tersebut dan akan tampil forum chat untuk costemer.</p>
+                  <p> * Ketika anda mengirim pesan dan akan mendapatkan balasan berupa arahan untuk memasukan email.</p>
+                    <p>* Pada tahapan ini anda boleh mengisi atau skip pengisian email tersebut.</p>
+            </p>
+           </div>
+           </div>
+           </div>
+         </section>   
+
+         
+         <!-- kategori-end -->
+
+         <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="fa9537ab-505a-4754-a5ba-b28154a23431";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
+         
+         
+        <div class="container text-center pt-5 pb-5">
+          All Rights Reserved &copy;2024
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
